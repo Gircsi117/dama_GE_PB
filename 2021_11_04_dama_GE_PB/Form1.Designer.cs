130cb7dce1884e5409add7ebac34b5cb7520676b
Gircsi117 <gilianerik117@gmail.com>
2021-11-04 16:24:40 +0100

    Alap elhelyezés

--- 2021_11_04_dama_GE_PB/Form1.Designer.cs
+++ 2021_11_04_dama_GE_PB/Form1.Designer.cs
@@ -31,25 +31,21 @@ namespace _2021_11_04_dama_GE_PB
         {
             this.exitBTN = new System.Windows.Forms.Button();
             this.alapPANEl = new System.Windows.Forms.Panel();
-            this.player_namePANEL_2 = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.player_namePANEL_1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.p2TBOX = new System.Windows.Forms.TextBox();
+            this.p1TBOX = new System.Windows.Forms.TextBox();
             this.cimLBL = new System.Windows.Forms.Label();
+            this.startBTN = new System.Windows.Forms.Button();
+            this.settingBTN = new System.Windows.Forms.Button();
+            this.infoBTN = new System.Windows.Forms.Button();
             this.alapPANEl.SuspendLayout();
-            this.player_namePANEL_2.SuspendLayout();
-            this.player_namePANEL_1.SuspendLayout();
             this.SuspendLayout();
             // 
             // exitBTN
             // 
-            this.exitBTN.AutoSize = true;
             this.exitBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.exitBTN.Location = new System.Drawing.Point(737, 25);
+            this.exitBTN.Location = new System.Drawing.Point(247, 331);
             this.exitBTN.Name = "exitBTN";
-            this.exitBTN.Size = new System.Drawing.Size(34, 23);
+            this.exitBTN.Size = new System.Drawing.Size(65, 23);
             this.exitBTN.TabIndex = 3;
             this.exitBTN.TabStop = false;
             this.exitBTN.Text = "Exit";
@@ -60,82 +56,45 @@ namespace _2021_11_04_dama_GE_PB
             // 
             this.alapPANEl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.alapPANEl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.alapPANEl.Controls.Add(this.player_namePANEL_2);
-            this.alapPANEl.Controls.Add(this.player_namePANEL_1);
+            this.alapPANEl.Controls.Add(this.exitBTN);
+            this.alapPANEl.Controls.Add(this.infoBTN);
+            this.alapPANEl.Controls.Add(this.settingBTN);
+            this.alapPANEl.Controls.Add(this.startBTN);
+            this.alapPANEl.Controls.Add(this.p1TBOX);
+            this.alapPANEl.Controls.Add(this.p2TBOX);
             this.alapPANEl.Controls.Add(this.cimLBL);
             this.alapPANEl.Location = new System.Drawing.Point(132, 25);
             this.alapPANEl.Name = "alapPANEl";
             this.alapPANEl.Size = new System.Drawing.Size(516, 457);
             this.alapPANEl.TabIndex = 2;
             // 
-            // player_namePANEL_2
+            // p2TBOX
             // 
-            this.player_namePANEL_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.player_namePANEL_2.Controls.Add(this.textBox2);
-            this.player_namePANEL_2.Controls.Add(this.label2);
-            this.player_namePANEL_2.Location = new System.Drawing.Point(305, 114);
-            this.player_namePANEL_2.Name = "player_namePANEL_2";
-            this.player_namePANEL_2.Size = new System.Drawing.Size(192, 100);
-            this.player_namePANEL_2.TabIndex = 2;
+            this.p2TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.p2TBOX.Location = new System.Drawing.Point(285, 146);
+            this.p2TBOX.Margin = new System.Windows.Forms.Padding(0);
+            this.p2TBOX.Name = "p2TBOX";
+            this.p2TBOX.Size = new System.Drawing.Size(191, 48);
+            this.p2TBOX.TabIndex = 1;
+            this.p2TBOX.TabStop = false;
+            this.p2TBOX.Text = "Player2";
+            this.p2TBOX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox2
+            // p1TBOX
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox2.Location = new System.Drawing.Point(-1, 50);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(0);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(191, 48);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TabStop = false;
-            this.textBox2.Text = "Player2";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(0, 0);
-            this.label2.Margin = new System.Windows.Forms.Padding(0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(190, 50);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Player 2:";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // player_namePANEL_1
-            // 
-            this.player_namePANEL_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.player_namePANEL_1.Controls.Add(this.textBox1);
-            this.player_namePANEL_1.Controls.Add(this.label1);
-            this.player_namePANEL_1.Location = new System.Drawing.Point(33, 115);
-            this.player_namePANEL_1.Name = "player_namePANEL_1";
-            this.player_namePANEL_1.Size = new System.Drawing.Size(192, 100);
-            this.player_namePANEL_1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(-1, 50);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(191, 48);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TabStop = false;
-            this.textBox1.Text = "Player1";
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 50);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Player 1:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.p1TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.p1TBOX.Location = new System.Drawing.Point(45, 146);
+            this.p1TBOX.Margin = new System.Windows.Forms.Padding(0);
+            this.p1TBOX.Name = "p1TBOX";
+            this.p1TBOX.Size = new System.Drawing.Size(191, 48);
+            this.p1TBOX.TabIndex = 1;
+            this.p1TBOX.TabStop = false;
+            this.p1TBOX.Text = "Player1";
+            this.p1TBOX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // cimLBL
             // 
+            this.cimLBL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.cimLBL.Location = new System.Drawing.Point(0, 0);
             this.cimLBL.Margin = new System.Windows.Forms.Padding(0);
             this.cimLBL.Name = "cimLBL";
@@ -144,24 +103,52 @@ namespace _2021_11_04_dama_GE_PB
             this.cimLBL.Text = "Dáma strike";
             this.cimLBL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // startBTN
+            // 
+            this.startBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.startBTN.Location = new System.Drawing.Point(247, 230);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(65, 23);
+            this.startBTN.TabIndex = 4;
+            this.startBTN.TabStop = false;
+            this.startBTN.Text = "Start";
+            this.startBTN.UseVisualStyleBackColor = true;
+            // 
+            // settingBTN
+            // 
+            this.settingBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.settingBTN.Location = new System.Drawing.Point(247, 259);
+            this.settingBTN.Name = "settingBTN";
+            this.settingBTN.Size = new System.Drawing.Size(65, 23);
+            this.settingBTN.TabIndex = 5;
+            this.settingBTN.TabStop = false;
+            this.settingBTN.Text = "Settings";
+            this.settingBTN.UseVisualStyleBackColor = true;
+            // 
+            // infoBTN
+            // 
+            this.infoBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.infoBTN.Location = new System.Drawing.Point(247, 288);
+            this.infoBTN.Name = "infoBTN";
+            this.infoBTN.Size = new System.Drawing.Size(65, 23);
+            this.infoBTN.TabIndex = 6;
+            this.infoBTN.TabStop = false;
+            this.infoBTN.Text = "Info";
+            this.infoBTN.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(867, 514);
-            this.Controls.Add(this.exitBTN);
             this.Controls.Add(this.alapPANEl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.alapPANEl.ResumeLayout(false);
-            this.player_namePANEL_2.ResumeLayout(false);
-            this.player_namePANEL_2.PerformLayout();
-            this.player_namePANEL_1.ResumeLayout(false);
-            this.player_namePANEL_1.PerformLayout();
+            this.alapPANEl.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -169,13 +156,12 @@ namespace _2021_11_04_dama_GE_PB
 
         private System.Windows.Forms.Button exitBTN;
         private System.Windows.Forms.Panel alapPANEl;
-        private System.Windows.Forms.Panel player_namePANEL_2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel player_namePANEL_1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox p2TBOX;
+        private System.Windows.Forms.TextBox p1TBOX;
         private System.Windows.Forms.Label cimLBL;
+        private System.Windows.Forms.Button startBTN;
+        private System.Windows.Forms.Button settingBTN;
+        private System.Windows.Forms.Button infoBTN;
     }
 }
 
--- 2021_11_04_dama_GE_PB/Form1.cs
+++ 2021_11_04_dama_GE_PB/Form1.cs
@@ -36,12 +36,12 @@ namespace _2021_11_04_dama_GE_PB
             alapPANEl.Location = new Point(this.Width / 2 - alapPANEl.Width / 2, 12);
 
             //exit button
-            exitBTN.FlatStyle = FlatStyle.Flat;
-            exitBTN.FlatAppearance.BorderColor = Color.FromArgb(192, 64, 0);
-            exitBTN.FlatAppearance.BorderSize = 1;
-            exitBTN.ForeColor = Color.FromArgb(192, 64, 0);
-            exitBTN.Font = new Font("Arial", Convert.ToInt32(Math.Round(magas * 0.02)));
-            exitBTN.Location = new Point(this.Width - (exitBTN.Width + 12), 12);
+            //exitBTN.FlatStyle = FlatStyle.Flat;
+            //exitBTN.FlatAppearance.BorderColor = Color.FromArgb(192, 64, 0);
+            //exitBTN.FlatAppearance.BorderSize = 1;
+            //exitBTN.ForeColor = Color.FromArgb(192, 64, 0);
+            //exitBTN.Font = new Font("Arial", Convert.ToInt32(Math.Round(magas * 0.02)));
+            //exitBTN.Location = new Point(this.Width - (exitBTN.Width + 12), 12);
 
             //cim
             cimLBL.BackColor = Color.FromArgb(50, 192, 64, 0);
@@ -49,24 +49,34 @@ namespace _2021_11_04_dama_GE_PB
             cimLBL.Font = new Font("Arial", cimLBL.Height/2);
             cimLBL.ForeColor = Color.FromArgb(192, 64, 0);
 
-            //pPanel_1
-            Panel[] p_panels = new Panel[2] { player_namePANEL_1, player_namePANEL_2 };
-            int[] pos = new int[2] {2*egyseg, 7*egyseg};
+            //textbox
+            TextBox[] p_panels = new TextBox[2] {p1TBOX, p2TBOX};
+            int[] pos = new int[2] { 2 * egyseg, 7 * egyseg };
+
             for (int i = 0; i < p_panels.Length; i++)
             {
-                p_panels[i].Size = new Size(egyseg*3, egyseg*2);
-                p_panels[i].Location = new Point(pos[i], egyseg*3);
+                p_panels[i].Location = new Point(pos[i], egyseg * 3);
+                p_panels[i].Size = new Size(egyseg*3, egyseg);
+                p_panels[i].Font = new Font("Arial", egyseg / 2);
 
-                //panel
-                p_panels[i].Controls[1].Location = new Point(0, 0);
-                p_panels[i].Controls[1].Size = new Size(p_panels[i].Width, egyseg);
-                p_panels[i].Controls[1].Font = new Font("Arial", egyseg / 2);
+            }
 
-                //textbox
-                p_panels[i].Controls[0].Location = new Point(0, egyseg);
-                p_panels[i].Controls[0].Size = new Size(p_panels[i].Width, egyseg);
-                p_panels[i].Controls[0].Font = new Font("Arial", egyseg / 2);
+            //menu buttons
+            Button[] menu_buttons = new Button[4] { startBTN, settingBTN, infoBTN, exitBTN};
+            int b_pos = 5;
 
+            for (int i = 0; i < menu_buttons.Length; i++)
+            {
+                menu_buttons[i].Size = new Size(egyseg * 4, egyseg);
+
+                menu_buttons[i].FlatStyle = FlatStyle.Flat;
+                menu_buttons[i].FlatAppearance.BorderColor = Color.FromArgb(192, 64, 0);
+                menu_buttons[i].FlatAppearance.BorderSize = 1;
+                menu_buttons[i].ForeColor = Color.FromArgb(192, 64, 0);
+                menu_buttons[i].Font = new Font("Arial", egyseg/2);
+
+                menu_buttons[i].Location = new Point(egyseg * 4, egyseg * b_pos);
+                b_pos += 2;
             }
 
             this.Focus();
