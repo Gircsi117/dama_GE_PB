782f7d0dfeed4ef32b6c634e40e8ef8e21d5b14b
Gircsi117 <gilianerik117@gmail.com>
2021-11-04 16:45:00 +0100

    Menu design

--- 2021_11_04_dama_GE_PB/Form1.Designer.cs
+++ 2021_11_04_dama_GE_PB/Form1.Designer.cs
@@ -31,12 +31,12 @@ namespace _2021_11_04_dama_GE_PB
         {
             this.exitBTN = new System.Windows.Forms.Button();
             this.alapPANEl = new System.Windows.Forms.Panel();
-            this.p2TBOX = new System.Windows.Forms.TextBox();
-            this.p1TBOX = new System.Windows.Forms.TextBox();
-            this.cimLBL = new System.Windows.Forms.Label();
-            this.startBTN = new System.Windows.Forms.Button();
-            this.settingBTN = new System.Windows.Forms.Button();
             this.infoBTN = new System.Windows.Forms.Button();
+            this.settingBTN = new System.Windows.Forms.Button();
+            this.startBTN = new System.Windows.Forms.Button();
+            this.p1TBOX = new System.Windows.Forms.TextBox();
+            this.p2TBOX = new System.Windows.Forms.TextBox();
+            this.cimLBL = new System.Windows.Forms.Label();
             this.alapPANEl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +55,6 @@ namespace _2021_11_04_dama_GE_PB
             // alapPANEl
             // 
             this.alapPANEl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.alapPANEl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.alapPANEl.Controls.Add(this.exitBTN);
             this.alapPANEl.Controls.Add(this.infoBTN);
             this.alapPANEl.Controls.Add(this.settingBTN);
@@ -63,22 +62,43 @@ namespace _2021_11_04_dama_GE_PB
             this.alapPANEl.Controls.Add(this.p1TBOX);
             this.alapPANEl.Controls.Add(this.p2TBOX);
             this.alapPANEl.Controls.Add(this.cimLBL);
-            this.alapPANEl.Location = new System.Drawing.Point(132, 25);
+            this.alapPANEl.Location = new System.Drawing.Point(12, 12);
             this.alapPANEl.Name = "alapPANEl";
             this.alapPANEl.Size = new System.Drawing.Size(516, 457);
             this.alapPANEl.TabIndex = 2;
             // 
-            // p2TBOX
+            // infoBTN
             // 
-            this.p2TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.p2TBOX.Location = new System.Drawing.Point(285, 146);
-            this.p2TBOX.Margin = new System.Windows.Forms.Padding(0);
-            this.p2TBOX.Name = "p2TBOX";
-            this.p2TBOX.Size = new System.Drawing.Size(191, 48);
-            this.p2TBOX.TabIndex = 1;
-            this.p2TBOX.TabStop = false;
-            this.p2TBOX.Text = "Player2";
-            this.p2TBOX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.infoBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.infoBTN.Location = new System.Drawing.Point(247, 288);
+            this.infoBTN.Name = "infoBTN";
+            this.infoBTN.Size = new System.Drawing.Size(65, 23);
+            this.infoBTN.TabIndex = 6;
+            this.infoBTN.TabStop = false;
+            this.infoBTN.Text = "Info";
+            this.infoBTN.UseVisualStyleBackColor = true;
+            // 
+            // settingBTN
+            // 
+            this.settingBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.settingBTN.Location = new System.Drawing.Point(247, 259);
+            this.settingBTN.Name = "settingBTN";
+            this.settingBTN.Size = new System.Drawing.Size(65, 23);
+            this.settingBTN.TabIndex = 5;
+            this.settingBTN.TabStop = false;
+            this.settingBTN.Text = "Settings";
+            this.settingBTN.UseVisualStyleBackColor = true;
+            // 
+            // startBTN
+            // 
+            this.startBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.startBTN.Location = new System.Drawing.Point(247, 230);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(65, 23);
+            this.startBTN.TabIndex = 4;
+            this.startBTN.TabStop = false;
+            this.startBTN.Text = "Start";
+            this.startBTN.UseVisualStyleBackColor = true;
             // 
             // p1TBOX
             // 
@@ -92,6 +112,18 @@ namespace _2021_11_04_dama_GE_PB
             this.p1TBOX.Text = "Player1";
             this.p1TBOX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // p2TBOX
+            // 
+            this.p2TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.p2TBOX.Location = new System.Drawing.Point(285, 146);
+            this.p2TBOX.Margin = new System.Windows.Forms.Padding(0);
+            this.p2TBOX.Name = "p2TBOX";
+            this.p2TBOX.Size = new System.Drawing.Size(191, 48);
+            this.p2TBOX.TabIndex = 1;
+            this.p2TBOX.TabStop = false;
+            this.p2TBOX.Text = "Player2";
+            this.p2TBOX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // cimLBL
             // 
             this.cimLBL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
@@ -103,45 +135,12 @@ namespace _2021_11_04_dama_GE_PB
             this.cimLBL.Text = "Dáma strike";
             this.cimLBL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // startBTN
-            // 
-            this.startBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.startBTN.Location = new System.Drawing.Point(247, 230);
-            this.startBTN.Name = "startBTN";
-            this.startBTN.Size = new System.Drawing.Size(65, 23);
-            this.startBTN.TabIndex = 4;
-            this.startBTN.TabStop = false;
-            this.startBTN.Text = "Start";
-            this.startBTN.UseVisualStyleBackColor = true;
-            // 
-            // settingBTN
-            // 
-            this.settingBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.settingBTN.Location = new System.Drawing.Point(247, 259);
-            this.settingBTN.Name = "settingBTN";
-            this.settingBTN.Size = new System.Drawing.Size(65, 23);
-            this.settingBTN.TabIndex = 5;
-            this.settingBTN.TabStop = false;
-            this.settingBTN.Text = "Settings";
-            this.settingBTN.UseVisualStyleBackColor = true;
-            // 
-            // infoBTN
-            // 
-            this.infoBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.infoBTN.Location = new System.Drawing.Point(247, 288);
-            this.infoBTN.Name = "infoBTN";
-            this.infoBTN.Size = new System.Drawing.Size(65, 23);
-            this.infoBTN.TabIndex = 6;
-            this.infoBTN.TabStop = false;
-            this.infoBTN.Text = "Info";
-            this.infoBTN.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(867, 514);
+            this.ClientSize = new System.Drawing.Size(1315, 694);
             this.Controls.Add(this.alapPANEl);
             this.Name = "Form1";
             this.Text = "Form1";
--- 2021_11_04_dama_GE_PB/Form1.cs
+++ 2021_11_04_dama_GE_PB/Form1.cs
@@ -29,54 +29,47 @@ namespace _2021_11_04_dama_GE_PB
         private void alap_general()
         {
             int magas = this.Height;
-            int egyseg = magas / 12;
+            int egyseg = magas / 24;
 
             //alap beállítása
             alapPANEl.Size = new Size(magas - 24, magas - 24);
             alapPANEl.Location = new Point(this.Width / 2 - alapPANEl.Width / 2, 12);
 
-            //exit button
-            //exitBTN.FlatStyle = FlatStyle.Flat;
-            //exitBTN.FlatAppearance.BorderColor = Color.FromArgb(192, 64, 0);
-            //exitBTN.FlatAppearance.BorderSize = 1;
-            //exitBTN.ForeColor = Color.FromArgb(192, 64, 0);
-            //exitBTN.Font = new Font("Arial", Convert.ToInt32(Math.Round(magas * 0.02)));
-            //exitBTN.Location = new Point(this.Width - (exitBTN.Width + 12), 12);
-
             //cim
             cimLBL.BackColor = Color.FromArgb(50, 192, 64, 0);
-            cimLBL.Size = new Size(magas, egyseg*2);
+            cimLBL.Size = new Size(magas, egyseg*4);
             cimLBL.Font = new Font("Arial", cimLBL.Height/2);
             cimLBL.ForeColor = Color.FromArgb(192, 64, 0);
 
             //textbox
             TextBox[] p_panels = new TextBox[2] {p1TBOX, p2TBOX};
-            int[] pos = new int[2] { 2 * egyseg, 7 * egyseg };
+            int[] pos = new int[2] { 4 * egyseg, 14 * egyseg };
 
             for (int i = 0; i < p_panels.Length; i++)
             {
-                p_panels[i].Location = new Point(pos[i], egyseg * 3);
-                p_panels[i].Size = new Size(egyseg*3, egyseg);
-                p_panels[i].Font = new Font("Arial", egyseg / 2);
+                p_panels[i].Size = new Size(egyseg*6, egyseg*2);
+                p_panels[i].Font = new Font("Arial", egyseg);
+                p_panels[i].Location = new Point(pos[i], egyseg * 6);
 
             }
 
             //menu buttons
             Button[] menu_buttons = new Button[4] { startBTN, settingBTN, infoBTN, exitBTN};
-            int b_pos = 5;
+            int b_pos = 10;
 
             for (int i = 0; i < menu_buttons.Length; i++)
             {
-                menu_buttons[i].Size = new Size(egyseg * 4, egyseg);
+                menu_buttons[i].Size = new Size(egyseg * 10, egyseg*2);
 
                 menu_buttons[i].FlatStyle = FlatStyle.Flat;
-                menu_buttons[i].FlatAppearance.BorderColor = Color.FromArgb(192, 64, 0);
+                menu_buttons[i].FlatAppearance.BorderColor = Color.FromArgb(255, 128, 0);
                 menu_buttons[i].FlatAppearance.BorderSize = 1;
-                menu_buttons[i].ForeColor = Color.FromArgb(192, 64, 0);
-                menu_buttons[i].Font = new Font("Arial", egyseg/2);
+                menu_buttons[i].ForeColor = Color.FromArgb(255, 128, 0);
+                menu_buttons[i].BackColor = Color.FromArgb(192, 64, 0);
+                menu_buttons[i].Font = new Font("Arial", egyseg);
 
-                menu_buttons[i].Location = new Point(egyseg * 4, egyseg * b_pos);
-                b_pos += 2;
+                menu_buttons[i].Location = new Point(egyseg * 7, egyseg * b_pos);
+                b_pos += 3;
             }
 
             this.Focus();
