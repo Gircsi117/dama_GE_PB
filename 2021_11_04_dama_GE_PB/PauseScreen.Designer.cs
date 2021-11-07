383920e42b3107c123f5461f29fbce6af15615d2
Gircsi117 <gilianerik117@gmail.com>
2021-11-07 13:20:58 +0100

    Pause screen létrehozása

---
+++ 2021_11_04_dama_GE_PB/PauseScreen.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace _2021_11_04_dama_GE_PB
+{
+    partial class PauseScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.centerPANEL = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // centerPANEL
+            // 
+            this.centerPANEL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerPANEL.Location = new System.Drawing.Point(127, 56);
+            this.centerPANEL.Name = "centerPANEL";
+            this.centerPANEL.Size = new System.Drawing.Size(475, 464);
+            this.centerPANEL.TabIndex = 0;
+            // 
+            // PauseScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Controls.Add(this.centerPANEL);
+            this.Name = "PauseScreen";
+            this.Size = new System.Drawing.Size(754, 601);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel centerPANEL;
+    }
+}
---
+++ 2021_11_04_dama_GE_PB/PauseScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2021_11_04_dama_GE_PB
+{
+    public partial class PauseScreen : UserControl
+    {
+        public PauseScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
